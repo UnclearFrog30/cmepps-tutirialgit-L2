f0f37d3d48069c4ebaf1c79486382566a8645932
clopq <clopq@DESKTOP-3DIA8P2>
2024-09-27 11:05:41 +0200

    Muestro el nombre.

--- src/tutorialGit/HolaMundo.java
+++ src/tutorialGit/HolaMundo.java
@@ -1,7 +1,12 @@
 package tutorialGit;
 
 public class HolaMundo {
-		public static void main() {
+		public static void main(String[] args) {
 			System.out.println("Hola CMEPPS!");
+			
+			System.out.println("Bienvendos al Curso");
+			
+			String name = "Christian";
+			System.out.println("Hola" + name);
 		}
 }
